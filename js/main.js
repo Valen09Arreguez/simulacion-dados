da24ee10995475e5be642274ca66ec3281bee8a6
Valentin Arreguez <arreguez.valentinelian@iesconcepcion.edu.ar>
2023-10-11 19:48:20 -0300

    Terminado el proyecto usando for

--- js/main.js
+++ js/main.js
@@ -2,13 +2,11 @@ let los2Dados =[];
 function numerosAleatorios(min, max) {
     return Math.floor(Math.random()*(max -min +1) +min);
 }
-
 function cantidadDeLanzamientos() {
     let cantidad = prompt("Ingresar la cantidad de lanzamientos");
     cantidad = parseInt(cantidad);
     return cantidad
 }
-
 function generar(cantidad) {
     const arrayDado1 = [];
     for (let i = 0; i < cantidad; i++) {
@@ -17,7 +15,6 @@ function generar(cantidad) {
     los2Dados.push(arrayDado1)
     return arrayDado1;    
 }
-
 function generar(cantidad) {
     const arrayDado2 = [];
     for (let i = 0; i < cantidad; i++) {
@@ -26,7 +23,18 @@ function generar(cantidad) {
     los2Dados.push(arrayDado2);
     return arrayDado2;    
 }
-
+function Sumatoria(array1, array2) {
+    let arraySuma = []
+    for (let i = 0; i < array1.length; i++) {
+        let suma = array1[i] + array2[i];
+        arraySuma.push(suma)
+    }
+    los2Dados.push(arraySuma);
+    return arraySuma;
+}
 const cantidadLanzamientos =  cantidadDeLanzamientos();
 arrayDado1 = generar(cantidadLanzamientos);
 arrayDado2 = generar(cantidadLanzamientos);
+Sumatoria(arrayDado1, arrayDado2);
+console.table(los2Dados);
+
